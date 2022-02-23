4db139afd929637216c7f1f733d52d3650421714
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-02-23 08:15:32 +0200

    Basic Auth

--- courseProject/src/main/java/org/elsys/ip/rest/QuestionController.java
+++ courseProject/src/main/java/org/elsys/ip/rest/QuestionController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/question")
+@RequestMapping("/api/question")
 public class QuestionController {
     private final QuestionRepository repository;
 
